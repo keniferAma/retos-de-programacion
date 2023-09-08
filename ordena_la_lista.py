de3c37947cbab78aff5ff57a8d0284ed801aae4d
kenifer <139505395+keniferAma@users.noreply.github.com>
2023-09-08 15:03:02 -0500

    ordena_la_lista completed + AI version.

--- ordena_la_lista.py
+++ ordena_la_lista.py
@@ -9,35 +9,35 @@
 
 def matriz(number_list: list, mode: str) -> list:
     result = []
-    number_list_2 = number_list
+
 
     if mode not in ["Asc", "Desc"]:
         return "Not allowed"
     
     if mode == "Asc":
-        while number_list_2:
+        while number_list:
             for n1 in number_list:
                 trues = 0
-                for n2 in number_list_2:
+                for n2 in number_list:
                     if n1 < n2:
                         trues += 1
 
                     if trues == len(number_list) - 1:
                         result.append(n1)
-                        number_list_2.remove(n1)
+                        number_list.remove(n1)
 
 
     if mode == "Desc":
-        while number_list_2:
+        while number_list:
             for n1 in number_list:
                 trues = 0
-                for n2 in number_list_2:
+                for n2 in number_list:
                     if n1 > n2:
                         trues += 1
 
                     if trues == len(number_list) - 1:
                         result.append(n1)
-                        number_list_2.remove(n1)
+                        number_list.remove(n1)
 
             
 
@@ -46,7 +46,26 @@ def matriz(number_list: list, mode: str) -> list:
 
 values = [4, 2, 6, 8, 7, 9, 1]
 
-
 print(matriz(values, "Desc"))
+
+
+
+# opcion hecha por inteligencia artificial:
+
+def ordenar_numeros(lista, orden):
+    n = len(lista)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if (orden == "Asc" and lista[j] > lista[j+1]) or (orden == "Desc" and lista[j] < lista[j+1]):
+                # Intercambiar elementos
+                temp = lista[j]
+                lista[j] = lista[j+1]
+                lista[j+1] = temp
+    return lista
+
+
+values = [4, 2, 6, 8, 7, 9, 1, 1]
+print(len(values))
+print(ordenar_numeros(values, "Asc"))
 
 
