04eccd5c6dfb290753739b72ad577a44fe13b731
kenifer <139505395+keniferAma@users.noreply.github.com>
2023-12-01 22:27:00 -0500

    analisis-de-texto challenge with one for loop in progress

--- analisis_de_texto.py
+++ analisis_de_texto.py
@@ -28,3 +28,46 @@ path = "C:/Users/kenifer/Desktop/pruebas-texto/analisis-de-texto-challenge.txt"
 
 print(analizador_de_texto(path=path))  
 
+
+
+## now we're gonna try with one only for loop ###
+
+def analizador_de_texto_v2(path: str) -> None:
+    try:
+        with open(path, "r+", encoding="utf-8") as archivo:
+            texto = archivo.read()
+    except FileNotFoundError as message:
+        raise message
+    
+    numero_total_de_palabras = len(texto.split())
+    longitud_palabras_individuales = []
+    
+    numero_de_oraciones = len(texto.split("."))
+    lista_limpia = []
+    palabra_mas_larga = 0
+
+    for i in texto.split():
+        longitud_palabras_individuales.append(len(i))
+
+        if i[-1] in ["!", "'", ".", ","]:
+            lista_limpia.append(i[:-1])
+        elif i[0] in [".", ":", ";", ",", "-", "¡", "'"]:
+            lista_limpia.append(i[1:])
+        else:
+            lista_limpia.append(i)
+
+
+    print(lista_limpia)
+
+    longitud_media_de_las_palabras = sum(longitud_palabras_individuales) / numero_total_de_palabras
+
+   
+    print(longitud_media_de_las_palabras)
+    print(numero_de_oraciones)
+    print(sorted(set(longitud_palabras_individuales), reverse=True))
+    
+
+    return numero_total_de_palabras
+
+
+print(analizador_de_texto_v2(path=path))                                                                                                     
