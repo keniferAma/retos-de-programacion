dfe6dd46e7e266890ab24c8a52d1650f304384d5
kenifer <139505395+keniferAma@users.noreply.github.com>
2023-12-07 13:49:04 -0500

    analisi-de-texto challenge completed by me (and working)

--- analisis_de_texto.py
+++ analisis_de_texto.py
@@ -25,8 +25,7 @@ def analizador_de_texto(path: str) -> None:
 
 
 path = "C:/Users/kenifer/Desktop/pruebas-texto/analisis-de-texto-challenge.txt"
-
-print(analizador_de_texto(path=path))  
+ 
 
 
 
@@ -36,6 +35,7 @@ def analizador_de_texto_v2(path: str) -> None:
     try:
         with open(path, "r+", encoding="utf-8") as archivo:
             texto = archivo.read()
+
     except FileNotFoundError as message:
         raise message
     
@@ -49,31 +49,30 @@ def analizador_de_texto_v2(path: str) -> None:
     
     
     for i in range(0, len(split)):
-        longitud_palabras_individuales.append(len(split[i]))
-
-        if len(palabra_mas_larga) == 0:
-            palabra_mas_larga.append(split[i])
-
+         
         if split[i][-1] in ["!", "'", ".", ","]:
             lista_limpia.append(split[i][:-1])
-            
         elif split[i][0] in ["¡", "'"]:
             lista_limpia.append(split[i][1:])
         else:
             lista_limpia.append(split[i])
 
-        if i > 1:
+        longitud_palabras_individuales.append(len(lista_limpia[i]))
+
+        if i >= 1:
             if len(lista_limpia[i]) > len(palabra_mas_larga[0]):
-                palabra_mas_larga.pop(0)
-                palabra_mas_larga.append(lista_limpia[i])
+                palabra_mas_larga = [lista_limpia[i]]
             elif len(lista_limpia[i]) == len(palabra_mas_larga[0]):
                 palabra_mas_larga.append(lista_limpia[i])
-        
-    longitud_media_de_las_palabras = sum(longitud_palabras_individuales) / numero_total_de_palabras
+        else:           
+            palabra_mas_larga.append(lista_limpia[i])
+                
+        longitud_media_de_las_palabras = sum(longitud_palabras_individuales) / numero_total_de_palabras
 
-    print(palabra_mas_larga)
-    
-    return numero_total_de_palabras
+    return {"numero total de palabras": numero_total_de_palabras,
+            "numero total de oraciones": numero_de_oraciones,
+            "palabra mas larga": ', '.join(palabra_mas_larga),
+            "longitud media de las palabras": longitud_media_de_las_palabras}
 
 
 print(analizador_de_texto_v2(path=path)) 
