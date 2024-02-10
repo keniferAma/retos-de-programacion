555c14349e66a31b2845756ec5782070e1cd443c
kenifer <139505395+keniferAma@users.noreply.github.com>
2024-02-10 16:42:21 -0500

    estructuras de datos: challenge done by brais.

--- estructuras_de_datos.py
+++ estructuras_de_datos.py
@@ -59,23 +59,88 @@ print(set(list_to_set))
 
 
 
-# extra #
+# EXTRA #
 from pydantic import BaseModel, Field
-entry = input('Describe the operation: ')
 
-db = {}
+
+db = []
 
 class Db(BaseModel):
     name: str
-    phone_number: int = Field(max_digits=11)
-
-
-# def excecution(operation: str):
-#     match operation:
-#         case 'search':
-#             for index,  in db.items()
+    cell_number: int = Field(lt=(11))
 
 
 
 
+def search(name: str):
+    """searching the current users"""
+    user_name = [n for n in db if n['name'] == name]
+    if not user_name:
+        return {'message': 'The user was not found.'}
+    return db[0]
 
+def insert(name: str, cell_phone: int):
+    if not search(name=name) == {'message': 'The user was not found.'}:
+        return {'message': 'The user is currently registered.'}
+    
+    db.append({'name': name, 'cell_number': cell_phone})
+    return {'message': 'user added correctly'}
+
+
+
+
+# by brais #
+def my_agenda():
+
+    agenda = {}
+
+    def insert_contact():
+        phone = input("Introduce el teléfono del contacto: ")
+        if phone.isdigit() and len(phone) > 0 and len(phone) <= 11:
+            agenda[name] = phone
+        else:
+            print(
+                "Debes introducir un número de teléfono un máximo de 11 dígitos.")
+
+    while True:
+
+        print("")
+        print("1. Buscar contacto")
+        print("2. Insertar contacto")
+        print("3. Actualizar contacto")
+        print("4. Eliminar contacto")
+        print("5. Salir")
+
+        option = input("\nSelecciona una opción: ")
+
+        match option:
+            case "1":
+                name = input("Introduce el nombre del contacto a buscar: ")
+                if name in agenda:
+                    print(
+                        f"El número de teléfono de {name} es {agenda[name]}.")
+                else:
+                    print(f"El contacto {name} no existe.")
+            case "2":
+                name = input("Introduce el nombre del contacto: ")
+                insert_contact()
+            case "3":
+                name = input("Introduce el nombre del contacto a actualizar: ")
+                if name in agenda:
+                    insert_contact()
+                else:
+                    print(f"El contacto {name} no existe.")
+            case "4":
+                name = input("Introduce el nombre del contacto a a eliminar: ")
+                if name in agenda:
+                    del agenda[name]
+                else:
+                    print(f"El contacto {name} no existe.")
+            case "5":
+                print("Saliendo de la agenda.")
+                break
+            case _:
+                print("Opción no válida. Elige una opción del 1 al 5.")
+
+
+my_agenda()
