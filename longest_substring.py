e88d259f5d1121078369bcbb13c490508a6adca4
kenifer <139505395+keniferAma@users.noreply.github.com>
2024-04-04 14:56:48 -0500

    longest_substring challenge: version by copilot

--- longest_substring.py
+++ longest_substring.py
@@ -59,3 +59,22 @@ def longest_substring(string: str):
 
 print(longest_substring('werwwwertuiwwwwww'))
 
+
+
+# code provided by copilot #
+
+def longest_substring(s: str) -> int:
+    start = 0
+    max_length = 0
+    used_char = {}
+
+    for i in range(len(s)):
+        if s[i] in used_char and start <= used_char[s[i]]:
+            start = used_char[s[i]] + 1
+        else:
+            max_length = max(max_length, i - start + 1)
+
+        used_char[s[i]] = i
+
+    return max_length
+
