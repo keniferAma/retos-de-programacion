115afd1c0ed305b33f81159453dcf5f2078e84b9
kenifer <139505395+keniferAma@users.noreply.github.com>
2024-04-16 14:50:28 -0500

    expresiones_regulares challenge: extra: email and phone number done

--- expresiones_regulares.py
+++ expresiones_regulares.py
@@ -24,3 +24,58 @@ print(concurrences)
 print(string_concurrences)
 print(''.join(string_concurrences))
 
+
+# extra challenge #
+
+# regular expression for email #
+
+import re
+
+email_string = 'kenifer@misena.edu'
+email_pattern = r'^[\w]{3,10}@(?:misena|gmail|outlook)(?:\.edu|\.com(?:\.co)?)?$'
+
+print(re.findall(email_pattern, email_string))
+
+"""
+about '?':
+
+In regular expressions, the ? symbol is a quantifier that makes the preceding element optional. 
+It means that the preceding element may appear zero or one time.
+
+When you see a ? at the end of a parenthesis, it applies to the entire group within the parenthesis. 
+For example, in the regular expression (abc)?, the ? makes the entire group abc optional. 
+This means the regular expression will match strings that contain abc and strings that do not contain abc.
+"""
+
+"""
+about non capturing ?:
+import re
+
+# Define a string and a regular expression with a non-capturing group
+string = "I like cats and I like dogs."
+regex = r"I like (?:cats|dogs)"  # The (?:...) defines a non-capturing group
+
+# Use re.findall() to find all matches
+matches = re.findall(regex, string)
+
+# Print the matches
+print(matches)  # Outputs: ['I like cats', 'I like dogs']
+"""
+
+
+match = re.match(email_pattern, 'alenadro@gmail.com')
+
+try:
+    print(match.group()) # the 'group' attribute return the matched concurrency. (works either with search and match)
+
+except AttributeError as message:
+    print(message)
+
+
+
+# regular expression for number phone #
+
+phone_string = '+57 3137576362'
+phone_pattern = r'(\+[0-9]{2,3})?\s?[0-9]{10}'
+
+print(re.search(phone_pattern, phone_string))
